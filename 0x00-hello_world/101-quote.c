403cc6d684b8b5c6490475243033c63c675a2c12
sazzy07 <inegbenose3@gmail.com>
2023-10-05 10:40:55 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line to the stadard error
+ * Return 1
+ */
+int main(void)
+{
+	fput("and that piece of art is iuseful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
